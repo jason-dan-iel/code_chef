a752392d5a0676599f8af304984b4ada84aa644f
Jason Daniel <jasondaniel18@gmail.com>
2022-10-09 23:45:17 +0530

    Solved Q3

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # code_chef
 Solved Q1 at 2300 8/10/2022
 Solved Q2
+Solved Q3 at 23:44 Oct/9/22
--- october_long_challenge/3.cpp
+++ october_long_challenge/3.cpp
@@ -2,30 +2,31 @@
 
 using namespace std;
 
-int A, B, speed_a, speed_b, t;
-int stepsa = 0;
-int stepsb = 0;
+int A, B, X, Y, t;
+
+// "Chef\n"
+// "Chefina\n"
+
+
 
 void solve(){
-    cin>>A>>B>>speed_a>>speed_b;
-    while(A>0){
-        stepsa+=1;
-        A-= speed_a;
-    }
-    while(B>0){
-        stepsb += 1;
-        B-= speed_b;
-    }
-    if(stepsb > stepsa)
-        cout<<"Chef\n";
-    else if (stepsb < stepsa)
-        cout<<"Chefina\n";
-    else 
+    cin>>A>>B>>X>>Y;
+    float steps_A = 0,steps_B = 0;
+    steps_A = (float)A/X;
+    steps_B = (float)B/Y;
+
+    // // cout<<steps_A<<" "<<steps_B<<endl;
+    // cout<<A<<" "<<B<<" "<<X<<" "<<Y<<" "<<steps_A<<" "<<steps_B<<endl;
+    if (steps_A == steps_B)
         cout<<"Both\n";
+    else if (steps_A > steps_B)
+        cout<<"Chefina\n";
+    else cout<<"Chef\n";
 }
 int main(){
     cin>>t;
-    while(t--)
+    while(t--){
         solve();
+    }
     return 0;
 }
